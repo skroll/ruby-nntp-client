3a4ec2350dd46a33e9300941a4e67d47fe96b145
Scott M. Kroll <skroll@gmail.com>
2012-12-13 18:21:45 -0500

    Add constants for article headers

---
+++ lib/nntp/headers.rb
@@ -0,0 +1,81 @@
+module NNTP
+  # Contains constants and methods for use in parsing headers for articles.
+  module Headers
+    # Mandatory Headers
+
+    # \Date of an article.
+    DATE       = 'Date'
+
+    # Author of an article.
+    FROM       = 'From'
+
+    # Unique message identifer.
+    MESSAGE_ID = 'Message-ID'
+  
+    # Newsgroups that the article is posted to.
+    NEWSGROUPS = 'Newsgroups'
+
+    # Route taken by an article.
+    PATH       = 'Path'
+
+    # Subject of an article.
+    SUBJECT    = 'Subject'
+
+
+    # Optional Headers
+
+    # Entities approving an article.
+    APPROVED       = 'Approved'
+
+    # Indication of the poster's intent regarding preservation of an article.
+    ARCHIVE        = 'Archive'
+
+    # Marks article as a control message.
+    CONTROL        = 'Control'
+
+    # Specifies geographic or organizational limits on an article's
+    # propagation.
+    DISTRIBUTION   = 'Distribution'
+
+    # The date and time that an article is no longer relevant.
+    EXPIRES        = 'Expires'
+
+    # Specifies which newsgroup(s) the poster has requested that followups
+    # are to be posted.
+    FOLLOWUP_TO    = 'Followup-To'
+
+    # The date and time the article was injected into the network.
+    INJECTION_DATE = 'Injection-Date'
+
+    # How the article was injected into the network.
+    INJECTION_INFO = 'Injection-Info'
+
+    # Identifies the article poster's organization.
+    ORGANIZATION   = 'Organization'
+
+    # Message's that are referenced in the article.
+    REFERENCES     = 'References'
+
+    # Short phrase summarizing the article's content.
+    SUMMARY        = 'Summary'
+
+    # Message identifier that this article supersedes.
+    SUPERSEDES     = 'Supersedes'
+
+    # The user agent used to generate the article.
+    USER_AGENT     = 'User-Agent'
+
+    # Where the article was filed by the news server.
+    XREF           = 'Xref'
+
+    # List of mandatory headers for an article.
+    MANDATORY = [DATE, FROM, MESSAGE_ID, NEWSGROUPS, PATH, SUBJECT]
+
+    # List of optional headers for an article.
+    OPTIONAL = [APPROVED, ARCHIVE, CONTROL, DISTRIBUTION, EXPIRES,
+      FOLLOWUP_TO, INJECTION_DATE, INJECTION_INFO, ORGANIZATION, REFERENCES,
+      SUMMARY, SUPERSEDES, USER_AGENT, XREF]
+  end
+end
+
+
